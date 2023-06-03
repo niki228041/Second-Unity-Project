5705645f78dd6e640755268d98c0efa267ea735a
niki228041 <lopocznikita@gmail.com>
2023-06-03 14:27:38 +0200

    all

---
+++ Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public int numClouds = 40;
+    public GameObject cloudPrefab;
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1;
+    public float cloudScaleMax = 3;
+    public float cloudSpeedMult = 0.5f;
+
+    private GameObject[] cloudInstances;
+
+    void Awake()
+    {
+        cloudInstances = new GameObject[numClouds];
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        // Создать в цикле заданное количество облаков
+        GameObject cloud;
+
+        for (int i = 0; i < numClouds; i++)
+        {
+            // Создать экземпляр cloudPrefab
+            cloud = Instantiate<GameObject>(cloudPrefab);
+            // Выбрать местоположение для облака
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+            // Масштабировать облако
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            // Меньшие облака (с меньшим значением scaleU) должны быть ближе
+            // к земле
+            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+            // Меньшие облака должны быть дальше
+            cPos.z = 100 - 90 * scaleU;
+            // Применить полученные значения координат и масштаба к облаку
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = Vector3.one * scaleVal;
+            // Сделать облако дочерним по отношению к anchor
+            cloud.transform.SetParent(anchor.transform);
+
+            cloudInstances[i] = cloud;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject cloud in cloudInstances)
+        {
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cPos = cloud.transform.position;
+
+            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
+            if (cPos.x <= cloudPosMin.x)
+            {
+                cPos.x = cloudPosMax.x;
+            }
+
+            cloud.transform.position = cPos;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Slighshot.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slighshot : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 30f;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
+
+    static private Slighshot S;
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+
+    private void Awake()
+    {
+        Transform transformPointTrans = transform.Find("LaunchPoint");
+        launchPoint = transformPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = transformPointTrans.position;
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Mouse is enter");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        print("Mouse is't entered");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = launchPos;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+
+    public void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
+    }
+}
